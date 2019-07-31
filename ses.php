3c5797e5aa802e1d19eef5e4de0df9be4199a8c8
man4toman <man4toman@gmail.com>
2019-07-31 13:28:23 +0430

    Upload to github

---
+++ ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  'e4dfbdaa68cff829d724148ca34344fd' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'en',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'root',
+      'my_pass' => '',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'cfg mysql',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1564561423,
+    'lng' => 'en',
+  ),
+);
+?>
